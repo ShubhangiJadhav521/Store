d3d3217852c45c075485b76d9b1d35e9587f68ec
shubhangi jadhav <shubhangijadhav521@gmail.com>
2023-12-09 12:24:57 +0530

    db change

--- server/db.js
+++ server/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connection = async()=>{
-  const URL = process.env.MONGO_URI;
+  const URL = `mongodb://0.0.0.0:27017/Stores`;
   try{
       await mongoose.connect(URL);
       console.log('database connected');
