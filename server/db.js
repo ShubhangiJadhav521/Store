f582bbc13ff76a32a420745d2d54a5981126ee52
shubhangi jadhav <shubhangijadhav521@gmail.com>
2023-12-09 11:57:55 +0530

    db Update

--- server/db.js
+++ server/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connection = async()=>{
   const URL = process.env.MONGO_URI;
   try{
-      await mongoose.connect(URL, {useNewUrlParser:true});
+      await mongoose.connect(URL);
       console.log('database connected');
   }catch(error){
       console.log('database not connected', error)
